c2ef0a7e1d559a63bca7687b883ee63c25b2f337
Loren Gordon <lorengordon@users.noreply.github.com>
2019-08-15 06:48:30 -0700

    Syncs salt python3 repo

--- repo/main.tf
+++ repo/main.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 locals {
-  rsync_command = [
+  rsync_base = [
     "rsync -vazmH --no-links --numeric-ids --delete --delete-excluded --delete-after",
     "--exclude \"*/SRPMS\"",
     "--exclude \"*/i386*\"",
@@ -13,20 +13,45 @@ locals {
     "--include \"*/\"",
     "${join(" ", local.salt_versions_include)}",
     "--exclude \"*\"",
-    "${var.salt_rsync_url} ${var.cache_dir}",
   ]
+
+  rsync_python2 = "${concat(
+    local.rsync_base,
+    list(var.salt_rsync_url, var.cache_dir))
+  }"
+
+  rsync_python3 = "${concat(
+    local.rsync_base,
+    list(var.salt_python3_rsync_url, "${var.cache_dir}/python3"))
+  }"
 }
 
 resource "null_resource" "pull" {
   count = "${local.skip_module ? 0 : 1}"
 
   provisioner "local-exec" {
-    command = "${join(" ", local.rsync_command)}"
+    command = "${join(" ", local.rsync_python2)}"
   }
 
   triggers {
     salt_versions = "${join(",", local.salt_versions)}"
   }
+}
+
+resource "null_resource" "pull_python3" {
+  count = "${local.skip_module ? 0 : 1}"
+
+  provisioner "local-exec" {
+    command = "${join(" ", local.rsync_python3)}"
+  }
+
+  triggers {
+    salt_versions = "${join(",", local.salt_versions)}"
+  }
+
+  depends_on = [
+    "null_resource.pull",
+  ]
 }
 
 locals {
@@ -59,5 +84,8 @@ resource "null_resource" "push" {
     s3_command    = "${join(" ", local.s3_command)}"
   }
 
-  depends_on = ["null_resource.pull"]
+  depends_on = [
+    "null_resource.pull",
+    "null_resource.pull_python3",
+    ]
 }
